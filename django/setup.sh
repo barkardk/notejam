7dfc717d9ed314e2668fdc7921f433c66c2cd78d
Kristin Barkardottir <kristin.barkardottir@netapp.com>
2022-06-20 22:31:48 +0000

    demo

--- django/setup.sh
+++ django/setup.sh
@@ -11,5 +11,22 @@ echo "install via helm , the values file or other parames can be used for variou
 helm install notejam notejam
 
 echo "Setup monitoring"
-helm install prometheus  prometheus-community/prometheus 
-helm install grafana  prometheus-community/grafana
+helm install grafana bitnami/grafana
+helm install loki bitnami/grafana-loki
+
+cat <<EOF 
+    1. Get the application URL by running these commands:
+    echo "Browse to http://127.0.0.1:8080"
+    kubectl port-forward svc/grafana 8080:3000 &
+
+2. Get the admin credentials:
+
+    echo "User: admin"
+    echo "Password: $(kubectl get secret grafana-admin --namespace default -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 -d)"
+
+    If this app was not so old I would have installed django-prometheus middleware and exported metrics to prometheus
+    that way
+    prometheus is also too large of a deployment for my small mini cluster in GKE  
+    so instead I enabled DEBUG mode in django , just to get some logs and shipped them to grafana loki
+    NOT what I would reccomend for a live a pplication but for a demo I guess it will do 
+EOF
